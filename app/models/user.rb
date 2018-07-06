ac9a033b782728bb742991097f97be53b971eb9c
DanL12186 <danlrcn@hotmail.com>
2018-07-06 14:16:04 -0400

    Added has_secure_password and some validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  validates :email, uniqueness: true, allow_blank: true
+  validates :password, length: { minimum: 8 }
+  validates :password, confirmation: true
+  validates :name, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
+
+  # def self.find_or_create_by_omniauth(auth_hash)  #placeholder for omniauth
+  #   where(email: auth_hash[:info][:email]).first_or_create do |user|
+  #     user.name = auth_hash[:info][:name]
+  #     user.password = SecureRandom.hex
+  #   end
+  # end
 end
