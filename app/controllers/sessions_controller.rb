a58c8200bf62a1af9c8d848e7621de29e2f1ec93
DanL12186 <danlrcn@hotmail.com>
2018-07-10 14:09:43 -0400

    Added a sessions controller and view (signing form)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,42 @@
+class SessionsController < ApplicationController
+
+  def new
+    redirect_if_logged_in
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: session_params[:name])
+    session[:user_id] = @user.id  if  @user && @user.authenticate(session_params[:password])
+    if !!session[:user_id]
+      redirect_to user_path(@user)
+    else
+      @user = User.new(name: session_params[:name])
+      flash[:error] = "Username and password don't match"
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+ #  def omnicreate
+ #   user = User.find_or_create_by_omniauth(auth)
+ #   session[:user_id] = user.id
+ #   redirect_to root_path #required to get OmniAuth to work right
+ # end
+
+  protected
+
+  # def auth
+  #  request.env['omniauth.auth']
+  # end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:password, :name)
+  end
+end
