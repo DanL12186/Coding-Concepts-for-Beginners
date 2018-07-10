e17b16ed6e4106a132eea817608bebf3bd5520ea
DanL12186 <danlrcn@hotmail.com>
2018-07-10 14:03:51 -0400

    Created users_controller
    added new/create/edit/show/update/destroy actions as well as strong
    params and a set_user! before_action method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+  before_action :set_user!, only: [:show, :edit, :update, :destroy]
+
+  def new
+    redirect_if_logged_in
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_user!
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation, :email)
+  end
 end
