8893999fc38ff47adf0fa9cc162535da9e9bfd1e
DanL12186 <danlrcn@hotmail.com>
2018-07-10 14:04:19 -0400

    added has_many :user_readings join table relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  has_many :user_readings
+
   validates :email, uniqueness: true, allow_blank: true
   validates :password, length: { minimum: 8 }
   validates :password, confirmation: true
