f5ec3b682e58f79064cea927bd65de84c313518e
DanL12186 <danlrcn@hotmail.com>
2018-07-16 11:32:45 -0400

    Created readings table with title, subtitle, cat_id, summary, content

--- db/migrate/20180707193155_create_readings.rb
+++ db/migrate/20180707193155_create_readings.rb
@@ -1,7 +1,11 @@
 class CreateReadings < ActiveRecord::Migration[5.2]
   def change
     create_table :readings do |t|
-
+      t.string :title, null: false
+      t.string :subtitle
+      t.integer :category_id
+      t.string :summary
+      t.text :content
       t.timestamps
     end
   end
