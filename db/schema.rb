115e82b0cef1a014acd4de8270ab63d964463182
DanL12186 <danlrcn@hotmail.com>
2018-08-06 17:12:48 -0400

    Added categories model/db, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_203206) do
+ActiveRecord::Schema.define(version: 2018_08_06_204254) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "subcategory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "readings", force: :cascade do |t|
     t.string "title", null: false
