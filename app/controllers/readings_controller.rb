139c248ca9d1b593805fa8763e239f201465c6f0
DanL12186 <danlrcn@hotmail.com>
2018-08-07 14:41:54 -0400

    added readings controller

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,2 +1,27 @@
 class ReadingsController < ApplicationController
+  before_action :set_reading!, only: [:show, :edit, :update, :destroy]
+
+  def new
+    redirect_to user_path(current_user) unless current_user.id == 1
+    @reading = Reading.new
+  end
+
+  def edit
+  end
+
+  def create
+    @reading = Reading.new(reading_params)
+    @reading.save ? (redirect_to reading_path(@reading)) : (render :new)
+  end
+
+  private
+
+  def set_reading!
+    @reading = Reading.find_by(params[:id])
+  end
+
+  def reading_params
+    params.require(:reading).permit(:title, :subtitle, :category_id, :summary, :content)
+  end
+
 end
